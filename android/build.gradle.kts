79449509df3c3fc3d923b15dc0009b9d37810178
huangfei123520 <97841922+huangfei123520@users.noreply.github.com>
2026-09-04 12:24:58 +0800

    ??? Android ?????? build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
